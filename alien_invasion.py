5b89768af68c62db2ebf437840cf08b01add53b1
babu97 <benkaimugul@gmail.com>
2023-06-26 21:31:41 +0300

    89j

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,7 +81,17 @@ class AlienInvasion:
         """Create the fleet of aliens."""
         #make an alien. 
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2*alien_width)
+        number_aliens_X = available_space_x // (2*alien_width)
+
+        #Create the first row of aliens. 
+        for alien_number in range(number_aliens_X):
+            #create an alien and place it in the row. 
+            alien =Alien(self)
+            alien.x = alien_width + 2 * alien_width*alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
             
 
