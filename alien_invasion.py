1523b7ebefcd897878e046144d211763b50b0dc3
babu97 <benkaimugul@gmail.com>
2023-06-29 10:50:39 +0300

    typing

--- alien_invasion.py
+++ alien_invasion.py
@@ -116,7 +116,7 @@ class AlienInvasion:
     def _create_alien(self,alien_number, row_number):
         """Create analien and place it in the row"""
         alien =Alien(self)
-        alien_width = alien.rect.width
+        #alien_width = alien.rect.width
         alien_width,alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width*alien_number
         alien.rect.x = alien.x
--- ship.py
+++ ship.py
@@ -37,8 +37,8 @@ class Ship:
           if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
         if self.moving_left:
-          if self.moving_left and self.rect.left > 0:
-            self.x -+ self.settings.ship_speed
+          if self.moving_left and self.rect.left >0:
+            self.x -=self.settings.ship_speed
         #update rect object from self.x.
         self.rect.x = self.x
         
